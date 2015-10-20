a08e4db18c6d3e53346c7a267c86b655f4c43d6a
kevbyte <kevin.schweigert@gmail.com>
2015-10-20 10:17:08 -0700

    work on profiles

--- client/app/standings/standings.js
+++ client/app/standings/standings.js
@@ -15,11 +15,27 @@ angular.module('86cup.standings', [])
         $scope.street.racers = resp.data.street;
         $scope.limited.racers = resp.data.limited;
         $scope.unlimited.racers = resp.data.unlimited;
-
       })
     };
 
     $scope.getRacerList()
+
+    $scope.redirectToUserStock = function(i) {
+      $window.localStorage.profiles = $scope.stock.racers[i].username;
+      $location.path('/profiles');
+    }
+    $scope.redirectToUserStreet = function(i) {
+      $window.localStorage.profiles = $scope.street.racers[i].username;
+      $location.path('/profiles');
+    }
+    $scope.redirectToUserLimited = function(i) {
+      $window.localStorage.profiles = $scope.limited.racers[i].username;
+      $location.path('/profiles');
+    }
+    $scope.redirectToUserUnlimited = function(i) {
+      $window.localStorage.profiles = $scope.unlimited.racers[i].username;
+      $location.path('/profiles');
+    }
     
   });
 
